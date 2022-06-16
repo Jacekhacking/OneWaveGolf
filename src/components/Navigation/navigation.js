bd679f6992d1c069c768c67117858f85ddea724a
jacekhacking <Jacekhacking@mac.com>
2022-06-16 10:21:01 -0600

    jp wedges navbar issue fixed

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -8,9 +8,9 @@ const navigation = () => {
 
             <div className={`${style['nav-container']} flex gradient-background`}>
                 <a href="#Home"><img className={style['nav-logo']} src={logo} alt="one wave logo"/></a>
-                <ul className={'flex'}>
+                <ul className={'flex'} style={{justifyContent:'center', alignItems:'center'}}>
                     <li className={style['nav-item']}><a href="#About">About</a></li>
-                    <li className={style['nav-item']}><a href="#Wedges">JP Wedges</a></li>
+                    <li className={style['nav-item']}><a href="#Wedges"> <p>JP Wedges</p></a></li>
                 </ul>
             </div>
 
